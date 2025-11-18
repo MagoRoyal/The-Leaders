e0052cd1a17384de29289ffb9fb556c816ede402
MagoRoyal <rodrigohalls2019@gmail.com>
2025-11-18 04:31:55 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Tema
-const toggle = document.getElementById("toggleTheme");
-toggle.onclick = () => document.body.classList.toggle("dark");
-
-// Formulário (protótipo funcional)
-document.getElementById("form").addEventListener("submit", function(e){
-    e.preventDefault();
-    document.getElementById("msg").innerText = "Formulário enviado! (Protótipo)";
-});
